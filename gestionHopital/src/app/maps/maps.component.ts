3debe6b81067bf002e7f5f17e11d9566c80c8c82
= <=>
2022-06-16 12:52:00 +0200

    Page RDV dynamicless

--- gestionHopital/src/app/maps/maps.component.ts
+++ gestionHopital/src/app/maps/maps.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 declare const google: any;
 
@@ -15,7 +16,7 @@ draggable?: boolean;
 })
 export class MapsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
 
